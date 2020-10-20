f7656218008090949e9c9fc060640132d1635f70
Level 51 <levelfiftyone@gmail.com>
2020-10-20 16:19:07 +0800

    you know what forget it lol

--- scripts/puzzle.js
+++ scripts/puzzle.js
@@ -30,25 +30,3 @@ function check(str, h) {
 		}
 	}
 }
-
-function check_live(str, h, n) {
-	if (localStorage.getItem('lph-solve') == null) {
-		localStorage.setItem('lph-solve', "00000000");
-	}
-	if (str.length > 0) {
-		if (hash(str) === h) {
-			document.getElementById("yes").innerHTML=
-				"<font color='green'><h3>✅ Solved!</h3>The answer was <strong>"
-				+ document.getElementById("checker").value.replace(/[^a-zA-Z ]/gi, "").toUpperCase()
-				+ "</strong>.";
-			var t = localStorage.getItem('lph-solve');
-			t = t.substr(0, n) + "1" + t.substr(n + 1);
-			localStorage.setItem('lph-solve', t);
-		} else {
-			document.getElementById("yes").innerHTML=
-				"<font color='#B00000'><h3>❌ Incorrect!</h3><strong>"
-				+ document.getElementById("checker").value.replace(/[^a-zA-Z ]/gi, "").toUpperCase()
-				+ "</strong> was not the answer.</font>";
-		}
-	}
-}
